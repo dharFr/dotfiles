1f8498f5fc96005778a73163cf43c68ca141fb68
Olivier Audard <audard@gmail.com>
2014-06-15 02:11:20 +0200

    Switch to Solarized color scheme for iTerm

--- osx-defaults.sh
+++ osx-defaults.sh
@@ -484,7 +484,7 @@ defaults write com.apple.terminal "Startup Window Settings" -string "Mathias"
 #defaults write org.x.X11 wm_ffm -bool true
 
 # Install pretty iTerm colors
-open "conf/thayer.itermcolors"
+open "conf/Solarized Dark.itermcolors"
 
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
