ae44912f929d419530ab3d475a1dc304262eb912
Olivier Audard <audard@gmail.com>
2016-08-17 09:37:47 +0200

    Update quick look plugins

--- osx-cask.sh
+++ osx-cask.sh
@@ -119,15 +119,16 @@ function main() {
 	brew cask install virtualbox
 
 	# OSX quick look plugins (from https://github.com/sindresorhus/quick-look-plugins)
-	brew cask install qlcolorcode
-	brew cask install qlstephen
-	brew cask install qlmarkdown
-	brew cask install quicklook-json
-	brew cask install qlprettypatch
-	brew cask install quicklook-csv
-	brew cask install webp-quicklook
+	brew cask install qlcolorcode 
+	brew cask install qlstephen 
+	brew cask install qlmarkdown 
+	brew cask install quicklook-json 
+	brew cask install qlprettypatch 
+	brew cask install quicklook-csv 
+	brew cask install betterzipql 
+	brew cask install qlimagesize 
+	brew cask install webpquicklook 
 	brew cask install suspicious-package
-
 
 	# Remove outdated versions from the cellar
 	brew cleanup
