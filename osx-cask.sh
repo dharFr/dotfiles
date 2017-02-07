6c8400613af8f808a877e32bdd720848c345a559
Olivier Audard <audard@gmail.com>
2017-02-07 12:03:40 +0100

    [setup] Call zin setup form external file

--- osx-cask.sh
+++ osx-cask.sh
@@ -61,18 +61,7 @@ function main() {
 	
 	# -- Install zim for ZSH ------------------------------------------------------------------
 	if [ ! -d "${ZDOTDIR:-${HOME}}/.zim" ]; then
-		print_info "Cloning zim repository to ${ZDOTDIR:-${HOME}}/.zim"
-		git clone --recursive https://github.com/Eriner/zim.git ${ZDOTDIR:-${HOME}}/.zim
-
-		print_info "prepend the initialization templates to configs"
-		setopt EXTENDED_GLOB
-		for template_file ( ${ZDOTDIR:-${HOME}}/.zim/templates/* ); do
-		  user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
-		  touch ${user_file}
-		  ( print -rn "$(<${template_file})$(<${user_file})" >! ${user_file} ) 2>/dev/null
-		done
-		
-		print_success "zim installed. Make sure to open a new terminal and run 'source \${ZDOTDIR:-\${HOME}}/.zlogin' to finish optimization"
+		$CWD/setup/zim-setup.zsh		
 	fi
 
 	# -- Homebrew ------------------------------------------------------------------
