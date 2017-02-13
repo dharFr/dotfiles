0ed17edd7f9c1baea68d3cf6fe7307eb25f7937f
Olivier Audard <audard@gmail.com>
2017-02-13 13:52:00 +0100

    [shell] Add CoreUtils to PATH

--- rc/exports.sh
+++ rc/exports.sh
@@ -25,3 +25,7 @@ which shopt > /dev/null && shopt -s histappend  # append to history, don't overw
 # Save and reload the history after each command finishes
 export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 # ^ the only downside with this is [up] on the readline will go over all history not just this bash session.
+
+# Required by CoreUtils (see macos-setup.sh)
+export PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
+
