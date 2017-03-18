103584255a86030b620ac332e06aa63560968846
Olivier Audard <olivier.audard@dailymotion.com>
2017-03-18 19:08:52 +0100

    [setup] Add npm-check-updates

--- setup/npm.sh
+++ setup/npm.sh
@@ -2,10 +2,11 @@
 
 # install usefull global packages
 npm install -g coffee-script
-npm install -g eslint
 npm install -g coffeelint
-npm install -g jsonlint
-npm install -g yo
 npm install -g emoj      # Fondamental! ♥  😍  😁  🎉  😂  😄  🙌
-npm install -g trash-cli # dependency for _Safeguard rm_ alias
+npm install -g eslint
 npm install -g gifsicle  # dependency for _mov2gif_ function
+npm install -g jsonlint
+npm install -g npm-check-updates
+npm install -g trash-cli # dependency for _Safeguard rm_ alias
+npm install -g yo
