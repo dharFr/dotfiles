875970a2dc2c3cd543a7e113f4db0c9d2d3026eb
Olivier Audard <audard@gmail.com>
2017-03-18 18:36:08 +0100

    [setup] Fix powerline font setup

--- setup/powerline-fonts.sh
+++ setup/powerline-fonts.sh
@@ -6,7 +6,7 @@ brew install python
 pip install --user powerline-status
 
 echo "Cloning powerline/fonts repository to ${HOME}/.fonts/powerline-fonts..."
-git clone git@github.com:powerline/fonts.git "${HOME}/.fonts/powerline-fonts"
+git clone https://github.com/powerline/fonts.git "${HOME}/.fonts/powerline-fonts"
 echo "Done."
 echo "You probably need to run Powerline fonts install script"
 echo "> cd ${HOME}/.fonts/powerline-fonts && ./install.sh"
